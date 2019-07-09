b38651b678f3dd0fcb63d1a3b4e48e75b7549afe
Yoshitaka Kimisaki <y.kimisaki@kimisaki.jp>
2019-07-09 19:08:42 +0900

    Offset対応した、テストはしてない

--- AirtableClient/AirtableClient/AirtableBase.cs
+++ AirtableClient/AirtableClient/AirtableBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
@@ -19,38 +20,47 @@ namespace Airtable
 
         public async Task<T[]> LoadTableAsync<T>()
         {
-            var url = $"https://api.airtable.com/v0/{baseId}/{typeof(T).Name}?api_key={client.ApiKey}";
-            var message = await client.Get(url);
-
-            if (message.StatusCode != HttpStatusCode.OK)
+            var result = new List<T>();
+            var offset = "0";
+            do
             {
-                switch (message.StatusCode)
+                var url = $"https://api.airtable.com/v0/{baseId}/{typeof(T).Name}?api_key={client.ApiKey}&offset={offset}";
+                var message = await client.Get(url);
+
+                if (message.StatusCode != HttpStatusCode.OK)
                 {
-                    case HttpStatusCode.BadRequest:
-                        throw new AirtableBadRequestException();
-                    case HttpStatusCode.Forbidden:
-                        throw new AirtableForbiddenException();
-                    case HttpStatusCode.NotFound:
-                        throw new AirtableNotFoundException();
-                    case HttpStatusCode.PaymentRequired:
-                        throw new AirtablePaymentRequiredException();
-                    case HttpStatusCode.Unauthorized:
-                        throw new AirtableUnauthorizedException();
-                    case HttpStatusCode.RequestEntityTooLarge:
-                        throw new AirtableRequestEntityTooLargeException();
-                    case (HttpStatusCode)422:
-                        var error = JsonSerializer.Deserialize<dynamic>(await message.Content.ReadAsByteArrayAsync());
-                        throw new AirtableInvalidRequestException(error?["error"]?["message"]);
-                    case (HttpStatusCode)429:
-                        throw new AirtableTooManyRequestsException();
-                    default:
-                        throw new AirtableUnrecognizedException(message.StatusCode);
+                    switch (message.StatusCode)
+                    {
+                        case HttpStatusCode.BadRequest:
+                            throw new AirtableBadRequestException();
+                        case HttpStatusCode.Forbidden:
+                            throw new AirtableForbiddenException();
+                        case HttpStatusCode.NotFound:
+                            throw new AirtableNotFoundException();
+                        case HttpStatusCode.PaymentRequired:
+                            throw new AirtablePaymentRequiredException();
+                        case HttpStatusCode.Unauthorized:
+                            throw new AirtableUnauthorizedException();
+                        case HttpStatusCode.RequestEntityTooLarge:
+                            throw new AirtableRequestEntityTooLargeException();
+                        case (HttpStatusCode)422:
+                            var error = JsonSerializer.Deserialize<dynamic>(await message.Content.ReadAsByteArrayAsync());
+                            throw new AirtableInvalidRequestException(error?["error"]?["message"]);
+                        case (HttpStatusCode)429:
+                            throw new AirtableTooManyRequestsException();
+                        default:
+                            throw new AirtableUnrecognizedException(message.StatusCode);
+                    }
                 }
+
+                var jsonBody = JsonSerializer.Deserialize<JsonBody<T>>(await message.Content.ReadAsByteArrayAsync());
+                offset = jsonBody.Offset;
+
+                result.AddRange(jsonBody.Records.Select(x => x.Body));
             }
+            while (!string.IsNullOrWhiteSpace(offset));
 
-            var jsonBody = JsonSerializer.Deserialize<JsonBody<T>>(await message.Content.ReadAsByteArrayAsync());
-
-            return jsonBody.Records.Select(x => x.Body).ToArray();
+            return result.ToArray();
         }
     }
 
